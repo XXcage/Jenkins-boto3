aa48d8f774532ab5188fd6b5f05212883dbd0ad2
XXcage <79426486+XXcage@users.noreply.github.com>
2023-03-29 04:17:31 +0300

    Update ec2InstanceDetails3.py

--- ec2InstanceDetails3.py
+++ ec2InstanceDetails3.py
@@ -3,19 +3,20 @@ import boto3
 # create an EC2 client
 ec2 = boto3.client('ec2')
 
-# get all instances and filter by running instances
+# get all instances and filter by stopped instances
 instances = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
 
-# check if there are no running instances
+# check if there are no stopped instances
 if len(instances['Reservations']) == 0:
     print('There are no stopped instances')
 else:
-    # print details of each running instance
+    # print details of each stopped instance
     for reservation in instances['Reservations']:
         for instance in reservation['Instances']:
             instance_id = instance['InstanceId']
             instance_name = ''
-            for tag in instance['Tags']:
-                if tag['Key'] == 'Name':
-                    instance_name = tag['Value']
+            if 'Tags' in instance:
+                for tag in instance['Tags']:
+                    if tag['Key'] == 'Name':
+                        instance_name = tag['Value']
             print(f'Instance ID: {instance_id} | Instance Name: {instance_name}')
