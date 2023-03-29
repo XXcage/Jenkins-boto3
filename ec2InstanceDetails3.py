14ea5260c44d7cb28838562059b6fe7ae055c494
XXcage <79426486+XXcage@users.noreply.github.com>
2023-03-29 04:13:47 +0300

    Update ec2InstanceDetails3.py

--- ec2InstanceDetails3.py
+++ ec2InstanceDetails3.py
@@ -1,20 +1,35 @@
 import boto3
 
-# Create an EC2 client
+# create an EC2 client
 ec2 = boto3.client('ec2')
 
-# Retrieve all EC2 instances
-response = ec2.describe_instances()
+# get all running instances
+response = ec2.describe_instances(
+    Filters=[
+        {
+            'Name': 'instance-state-name',
+            'Values': ['running']
+        }
+    ]
+)
 
-# Loop through all instances
+# loop through instances
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
+        # check if there are any tags associated with the instance
+        if 'Tags' in instance:
+            for tag in instance['Tags']:
+                if tag['Key'] == 'Name':
+                    instance_name = tag['Value']
+                    break
+            else:
+                instance_name = 'N/A'
+        else:
+            instance_name = 'N/A'
+        
         instance_id = instance['InstanceId']
-        instance_state = instance['State']['Name']
-        instance_name = ''
-        for tag in instance['Tags']:
-            if tag['Key'] == 'Name':
-                instance_name = tag['Value']
-        # Check if the instance is in a stopped state
-        if instance_state == 'stopped':
-            print('Instance ID: {} | Instance Name: {}'.format(instance_id, instance_name))
+        instance_type = instance['InstanceType']
+        private_ip = instance['PrivateIpAddress']
+        public_ip = instance.get('PublicIpAddress', 'N/A')
+        
+        print(f'Instance ID: {instance_id} | Instance Name: {instance_name} | Instance Type: {instance_type} | Private IP: {private_ip} | Public IP: {public_ip}')
