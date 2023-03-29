4da1ad89963c457a2b9ef344c13ea2864f93ecb9
XXcage <79426486+XXcage@users.noreply.github.com>
2023-03-29 04:15:17 +0300

    Update ec2InstanceDetails3.py

--- ec2InstanceDetails3.py
+++ ec2InstanceDetails3.py
@@ -3,33 +3,19 @@ import boto3
 # create an EC2 client
 ec2 = boto3.client('ec2')
 
-# get all running instances
-response = ec2.describe_instances(
-    Filters=[
-        {
-            'Name': 'instance-state-name',
-            'Values': ['running']
-        }
-    ]
-)
+# get all instances and filter by running instances
+instances = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
-# loop through instances
-for reservation in response['Reservations']:
-    for instance in reservation['Instances']:
-        # check if there are any tags associated with the instance
-        if 'Tags' in instance:
+# check if there are no running instances
+if len(instances['Reservations']) == 0:
+    print('There are no running instances')
+else:
+    # print details of each running instance
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            instance_name = ''
             for tag in instance['Tags']:
                 if tag['Key'] == 'Name':
                     instance_name = tag['Value']
-                    break
-            else:
-                instance_name = 'N/A'
-        else:
-            instance_name = 'N/A'
-        
-        instance_id = instance['InstanceId']
-        instance_type = instance['InstanceType']
-        private_ip = instance['PrivateIpAddress']
-        public_ip = instance.get('PublicIpAddress', 'N/A')
-        
-        print(f'Instance ID: {instance_id} | Instance Name: {instance_name} | Instance Type: {instance_type} | Private IP: {private_ip} | Public IP: {public_ip}')
+            print(f'Instance ID: {instance_id} | Instance Name: {instance_name}')
