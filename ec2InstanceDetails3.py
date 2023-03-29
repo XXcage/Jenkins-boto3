f111b57da50e59fa44c0fca2f988090e31b7ac1f
XXcage <79426486+XXcage@users.noreply.github.com>
2023-03-29 03:06:41 +0300

    Create ec2InstanceDetails3.py

---
+++ ec2InstanceDetails3.py
@@ -0,0 +1,20 @@
+import boto3
+
+# Create an EC2 client
+ec2 = boto3.client('ec2')
+
+# Retrieve all EC2 instances
+response = ec2.describe_instances()
+
+# Loop through all instances
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        instance_id = instance['InstanceId']
+        instance_state = instance['State']['Name']
+        instance_name = ''
+        for tag in instance['Tags']:
+            if tag['Key'] == 'Name':
+                instance_name = tag['Value']
+        # Check if the instance is in a stopped state
+        if instance_state == 'stopped':
+            print(f'Instance ID: {instance_id} | Instance Name: {instance_name}')
