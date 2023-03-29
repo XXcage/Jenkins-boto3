32ea59633ead55c76fdabf6971bc991e7266f221
XXcage <79426486+XXcage@users.noreply.github.com>
2023-03-29 04:25:56 +0300

    Update and rename ec2InstanceDetails2.py to ec2InstanceDetails-json.py

---
+++ ec2InstanceDetails-json.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+
+# create an EC2 client
+ec2 = boto3.client('ec2')
+
+# get all instances and filter by stopped instances
+instances = ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']}])
+
+# check if there are no stopped instances
+if len(instances['Reservations']) == 0:
+    print('There are no stopped instances')
+else:
+    # extract details of each stopped instance and store in a list
+    stopped_instances = []
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            instance_id = instance['InstanceId']
+            instance_name = ''
+            for tag in instance['Tags']:
+                if tag['Key'] == 'Name':
+                    instance_name = tag['Value']
+            instance_details = {
+                'InstanceId': instance_id,
+                'InstanceName': instance_name,
+                'State': instance['State']['Name'],
+                'PrivateIpAddress': instance['PrivateIpAddress'],
+                'LaunchTime': str(instance['LaunchTime'])
+            }
+            stopped_instances.append(instance_details)
+
+    # output list of stopped instances in JSON format
+    print(json.dumps(stopped_instances, indent=4))
