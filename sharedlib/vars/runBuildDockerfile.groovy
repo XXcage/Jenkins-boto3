82a1359a65008503f1c5a8e93c69f434f7c312ec
XXcage <79426486+XXcage@users.noreply.github.com>
2023-04-04 07:45:08 +0300

    Update runBuildDockerfile.groovy

--- sharedlib/vars/runBuildDockerfile.groovy
+++ sharedlib/vars/runBuildDockerfile.groovy
@@ -6,52 +6,44 @@ def call() {
         awsPath = "/var/jenkins_home/.aws"
         }
         stages {
-        stage('Checkout+aws creds') {
-            steps {
-            cleanWs()
-            sh 'git clone -b main https://github.com/XXcage/Proj3.git'
-            sh "cp -r ${env.awsPath} Proj3/"
-    
+            stage('Checkout+aws creds') {
+                steps {
+                    cleanWs()
+                    sh 'git clone -b main https://github.com/XXcage/Proj3.git'
+                    sh "cp -r ${env.awsPath} Proj3/"
+                }
             }
-        }
-        stage('Build Docker Image') {
-            steps {
-            dir('Proj3') {
-                script {
-                sh "pwd"
-                sh "docker build -t ${env.IMAGE_NAME} ."
-                }      
+            stage('Build Docker Image') {
+                steps {
+                    dir('Proj3') {
+                        script {
+                            sh "pwd"
+                            sh "docker build -t ${env.IMAGE_NAME} ."
+                        }      
+                    }
+                }
             }
+        
+            stage('DockerHub Login and push') {
+                steps {
+                    withCredentials([usernamePassword(credentialsId: 'DockerHubID', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
+                        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+                        sh "docker push ${env.IMAGE_NAME}"
+                    }
+                }
             }
-        }
-    
-        stage('DockerHub Login and push') {
+            stage('Push Docker Image') {
             steps {
-            withCredentials([usernamePassword(credentialsId: 'DockerHubID', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-                sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
                 sh "docker push ${env.IMAGE_NAME}"
             }
             }
-        }
-        // stage('Push Docker Image') {
-        //   steps {
-        //     sh "docker push ${env.IMAGE_NAME}"
-        //   }
-        // }
-        stage('Run cat pylint output') {
-            steps {
-            script {
-                sh "docker run ${env.IMAGE_NAME} cat pylint-output.txt"
-            }
-            }
-        }
-        stage('Run Docker Image') {
-            steps {
-            script {
-                sh "docker run --rm ${env.IMAGE_NAME}"
-            }
-            }
-        } 
+            // stage('Run cat pylint output') {
+            //     steps {
+            //     script {
+            //         sh "docker run ${env.IMAGE_NAME} cat pylint-output.txt"
+            //     }
+            //     }
+            // }
         }
     }
 }
