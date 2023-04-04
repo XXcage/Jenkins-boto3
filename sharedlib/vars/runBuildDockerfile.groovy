5a993832e248228d03d1d8dc5a2f744567b8f0cd
XXcage <79426486+XXcage@users.noreply.github.com>
2023-04-04 09:04:03 +0300

    Update runBuildDockerfile.groovy

--- sharedlib/vars/runBuildDockerfile.groovy
+++ sharedlib/vars/runBuildDockerfile.groovy
@@ -1,42 +1,46 @@
 def call() {    
     pipeline {
-        agent any
-        environment {
+      agent any
+      environment {
         IMAGE_NAME = 'rzlinux0/proj3:${BUILD_NUMBER}'
         awsPath = "/var/jenkins_home/.aws"
+      }
+      stages {
+        stage('Checkout') {
+          steps {
+            cleanWs()
+            sh 'git clone -b main https://github.com/XXcage/Proj3.git'
+
+          }
         }
-        stages {
-            stage('Checkout+aws creds') {
-                steps {
-                    cleanWs()
-                    sh 'git clone -b main https://github.com/XXcage/Proj3.git'
-                    sh "cp -r ${env.awsPath} Proj3/"
-                }
+        stage('Build Docker Image') {
+          steps {
+            //copy aws creds for docker to be able to run .py using boto
+            sh "cp -r ${env.awsPath} Proj3/"
+            dir('Proj3') {
+              script {
+                sh "docker build -t ${env.IMAGE_NAME} ."
+              }      
             }
-            stage('Build Docker Image') {
-                steps {
-                    dir('Proj3') {
-                        script {
-                            sh "pwd"
-                            sh "docker build -t ${env.IMAGE_NAME} ."
-                        }      
-                    }
-                }
-            }
-        
-            stage('DockerHub Login and push') {
-                steps {
-                    withCredentials([usernamePassword(credentialsId: 'DockerHubID', passwordVariable: 'DOCKER_PASSWORD', usernameVariable: 'DOCKER_USERNAME')]) {
-                        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
-                        sh "docker push ${env.IMAGE_NAME}"
-                    }
-                }
-            }
-            stage('Push Docker Image') {
+          }
+        }
+
+        stage('DockerHub push') {
             steps {
-                sh "docker push ${env.IMAGE_NAME}"
+                script {
+                    // Read the DockerHub credentials from the properties file
+                    def dockerhubCredentials = readFile('/var/jenkins_home/.dockerhub/dockerhub-credentials.properties').trim()
+                    // Split the contents of the file into an array of lines
+                    def credentials = dockerhubCredentials.tokenize("\n")
+                    // Set the DockerHub username and password as environment variables
+                    env.DOCKERHUB_USERNAME = credentials[0].trim().split("=")[1].trim()
+                    env.DOCKERHUB_PASSWORD = credentials[1].trim().split("=")[1].trim()
+                    sh "docker login -u ${env.DOCKERHUB_USERNAME} -p ${env.DOCKERHUB_PASSWORD} https://index.docker.io/v1/"
+                    sh "docker push ${env.IMAGE_NAME}"
+                }
             }
-            }
+        }
+
             // stage('Run cat pylint output') {
             //     steps {
             //     script {
